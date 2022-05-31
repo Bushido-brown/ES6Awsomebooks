e4a82cb963d4c09ff92f2cd6c024c8c83f7953d6
Bushido-brown <orizut@gmail.com>
2022-05-31 09:34:53 -0700

    impliment ES6 syntax

--- Assets/JavaScript/index.js
+++ Assets/JavaScript/index.js
@@ -1,25 +1,25 @@
-import Nav from '../../modules/Nav.js';
-import Books from '../../modules/Books.js';
-import { DateTime } from '../../node_modules/luxon/src/luxon.js';
+import Nav from "../../modules/Nav.js";
+import Books from "../../modules/Books.js";
+import { DateTime } from "../../node_modules/luxon/src/luxon.js";
 
 const awesomeBooks = new Books();
-const form = document.querySelector('#add-book');
-const navMenu = document.querySelectorAll('.navBtn');
+const form = document.querySelector("#add-book");
+const navMenu = document.querySelectorAll(".navBtn");
 
-form.addEventListener('submit', (event) => {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const bookTitle = document.querySelector('#title');
-  const bookAutor = document.querySelector('#autor');
+  const bookTitle = document.querySelector("#title");
+  const bookAutor = document.querySelector("#autor");
 
-  if (bookTitle.value.trim() !== '' && bookAutor.value.trim() !== '') {
+  if (bookTitle.value.trim() !== "" && bookAutor.value.trim() !== "") {
     const bookValues = {
       autor: bookAutor.value,
       title: bookTitle.value,
     };
     awesomeBooks.newCollection(bookValues);
     awesomeBooks.showBooks();
-    bookTitle.value = '';
-    bookAutor.value = '';
+    bookTitle.value = "";
+    bookAutor.value = "";
   }
 });
 
@@ -27,16 +27,15 @@ const remove = (index) => {
   awesomeBooks.deleteCollection(index);
 };
 navMenu.forEach((nav) => {
-  nav.addEventListener('click', () => {
-    Nav (nav.getAttribute('id'));
+  nav.addEventListener("click", () => {
+    Nav(nav.getAttribute("id"));
   });
 });
-
 remove(-1);
 awesomeBooks.showBooks();
-const removeButton = document.querySelectorAll('button.removebtn');
+const removeButton = document.querySelectorAll("button.removebtn");
 removeButton.forEach((btn) => {
-  btn.addEventListener('click', () => {
+  btn.addEventListener("click", () => {
     remove(btn.dataset.index);
   });
 });
@@ -50,5 +49,5 @@ setInterval(() => {
   const showDate = `${now.toLocaleString(DateTime.DATE_FULL)} ${
     now.hour
   }:${twoDigits(now.minute)}:${twoDigits(now.second)} hrs.`;
-  document.querySelector('.showDate').innerHTML = showDate;
+  document.querySelector(".showDate").innerHTML = showDate;
 }, 1000);
--- modules/Nav.js
+++ modules/Nav.js
@@ -1,15 +1,15 @@
 const Nav = (sectionId) => {
-  const navSections = document.querySelectorAll('section');
-  const linkMenu = document.querySelectorAll('.navBtn');
+  const navSections = document.querySelectorAll("section");
+  const linkMenu = document.querySelectorAll(".navBtn");
   navSections.forEach((section) => {
-    section.style.display = 'none';
-   });
-  linkMenu.forEach((menu) => {
-    menu.classList.remove('active');
+    section.style.display = "none";
   });
-  
-  document.querySelector(`#${sectionId}`).classList.add('active');
-  document.querySelector(`.${sectionId}`).style.display = 'flex';
-  };
-  
+  linkMenu.forEach((menu) => {
+    menu.classList.remove("active");
+  });
+
+  document.querySelector(`#${sectionId}`).classList.add("active");
+  document.querySelector(`.${sectionId}`).style.display = "flex";
+};
+
 export default Nav;
