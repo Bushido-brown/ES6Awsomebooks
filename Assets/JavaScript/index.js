229d7f7738a48d061da6e6cad2e779bb12631667
Bushido-brown <orizut@gmail.com>
2022-05-31 21:02:41 -0700

    impliment ES6 syntax

--- Assets/JavaScript/index.js
+++ Assets/JavaScript/index.js
@@ -1,10 +1,12 @@
 import Nav from '../../modules/Nav.js';
 import Books from '../../modules/Books.js';
-import { DateTime } from '../../modules/src/luxon.js';
+import { DateTime } from '../../modules/luxton.js';
 
 const awesomeBooks = new Books();
 const form = document.querySelector('#add-book');
 const navMenu = document.querySelectorAll('.navBtn');
+const date = document.querySelector('.date');
+
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -23,31 +25,17 @@ form.addEventListener('submit', (event) => {
   }
 });
 
-const remove = (Index) => {
-  awesomeBooks.deleteCollection(Index);
-};
 navMenu.forEach((nav) => {
   nav.addEventListener('click', () => {
     Nav(nav.getAttribute('id'));
   });
 });
-remove(-1);
-awesomeBooks.showBooks();
-const removeButton = document.querySelectorAll('button.removebtn');
-removeButton.forEach((btn) => {
-  btn.addEventListener('click', () => {
-    remove(btn.dataset.Index);
-  });
-});
-const twoDigits = (num) => {
-  if (num < 10) return `0${num}`;
-  return num;
-};
 
-setInterval(() => {
-  const now = DateTime.now();
-  const showDate = `${now.toLocaleString(DateTime.DATE_FULL)} ${
-    now.hour
-  }:${twoDigits(now.minute)}:${twoDigits(now.second)} hrs.`;
-  document.querySelector('.showDate').innerHTML = showDate;
-}, 1000);
+let getDate = DateTime.now();
+date.innerHTML = `${getDate.monthLong} ${getDate.day} ${getDate.year}, ${getDate.hour}:${getDate.minute}:${getDate.second}`;
+// Update the date every second
+const updateDate = () => {
+  getDate = DateTime.now();
+  date.innerHTML = `${getDate.monthLong} ${getDate.day} ${getDate.year}, ${getDate.hour}:${getDate.minute}:${getDate.second}`;
+};
+setInterval(updateDate, 1000);
--- modules/Books.js
+++ modules/Books.js
@@ -23,33 +23,35 @@ export default class Books {
     document.querySelector('#show-books').style.display = 'flex';
   };
 
-  deleteCollection = (bookIndex) => {
-    let bookRemoved;
-    if (bookIndex !== null) {
-      const books = this.#getBooksList();
-      bookRemoved = books.filter((_item, key) => {
-        if (key === parseInt(bookIndex, 10)) {
-          return null;
-        }
-        return true;
-      });
-      this.#SetBooksList(bookRemoved);
-      this.showBooks();
-    }
-    
-  };
-
   showBooks = () => {
     const books = this.#getBooksList();
     const htmlbooks = document.querySelector('.books');
     htmlbooks.innerHTML = '';
     for (let i = 0; i < books.length; i += 1) {
-      htmlbooks.innerHTML += `
-          <div class='book'>
-          <h3>'${books[i].title}'</h3>
-          <p><span class='by-span'> By </span> ${books[i].autor}</p>
-          <button type='button' data-index='${i}' class='removebtn'>Remove</button>
-          `;
+        const wrapper = document.createElement('div');
+        const info = document.createElement('p');
+        
+        const button = document.createElement('button')
+
+        htmlbooks.appendChild(wrapper);
+        wrapper.append(info)
+        wrapper.appendChild(button)
+
+        info.textContent = `${books[i].title} authored by  ${books[i].autor}`;
+        button.textContent ='remove';
+        button.addEventListener('click', this.deleteButton.bind(this));
+        button.setAttribute('data', i);
+        
+
+
     }
   };
+  deleteButton = (evt)=>{
+    
+    const allBooks = this.#getBooksList();
+    allBooks.splice(evt.currentTarget.getAttribute('data'), 1);
+    this.showBooks(allBooks);
+    this.#SetBooksList(allBooks);
+    console.log('click')
+  }
 }
